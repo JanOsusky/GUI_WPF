e77721ba5f05f2d1f9db83d7b5c6cb421078a802
JanOsusky <129309495+JanOsusky@users.noreply.github.com>
2023-10-31 21:19:50 +0100

    Final app

---
+++ WPFWeather.App/WPFWeather.App/Windows/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WPFWeather.App.Services;
+using WPFWeather.App.ViewModels;
+
+namespace WPFWeather.App.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WeatherViewModel viewmodel = new WeatherViewModel(new WeatherDiskService());
+            DataContext = viewmodel;
+            viewmodel.DownloadWeatherCommand.Execute("Brno");
+        }
+    }
+}
